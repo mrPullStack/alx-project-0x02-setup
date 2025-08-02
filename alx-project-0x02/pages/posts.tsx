270e5f83f948c382ed60a94d86f8247ce8fdd6b8
sungu <cliffland.jasungu@gmail.com>
2025-08-02 12:01:33 +0300

    update pages/posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,7 +1,22 @@
 import Head from 'next/head';
 import Header from '@/components/layout/Header';
+import { useEffect, useState } from 'react';
+import PostCard from '@/components/common/PostCard';
+import { type PostProps } from '@/interfaces';
 
 export default function PostsPage() {
+    const [posts, setPosts] = useState<PostProps[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        .then((res) => res.json())
+        .then((data) => {
+            setPosts(data);
+            setLoading(false);
+        });
+    }, []);
+
     return (
         <>
         <Head>
@@ -11,7 +26,14 @@ export default function PostsPage() {
             <Header />
             <div className="p-8 text-center">
             <h1 className="text-3xl font-bold">Posts Page</h1>
-            <p className="mt-4 text-gray-700">This page will display posts fetched from an external API.</p>
+            <p className="mt-4 text-gray-700">This page displays posts fetched from an external API.</p>
+            </div>
+            <div className="px-8">
+            {loading ? (
+                <p className="text-center text-gray-500">Loading posts...</p>
+            ) : (
+                posts.map((post) => <PostCard key={post.id} {...post} />)
+            )}
             </div>
         </main>
         </>
